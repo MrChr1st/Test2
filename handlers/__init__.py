713217d02a39b2b58f015c9f4c3c8a8bb0464165
MrChr1st <103982021+MrChr1st@users.noreply.github.com>
2026-03-07 20:10:11 +0300

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,3 @@
-from aiogram import Router
-from .user import router as user_router
-from .admin import router as admin_router
 
+from aiogram import Router
 router = Router()
-router.include_router(user_router)
-router.include_router(admin_router)
