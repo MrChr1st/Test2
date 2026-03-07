1c7b76eef258bebf0bdc97ebce2a435e480971e9
MrChr1st <103982021+MrChr1st@users.noreply.github.com>
2026-03-07 20:38:14 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,22 @@
-
 import sqlite3
 
+
 class Database:
-    def __init__(self,path):
-        self.path=path
+
+    def __init__(self, path):
+        self.path = path
+        self.init()
+
+    def init(self):
+        conn = sqlite3.connect(self.path)
+        c = conn.cursor()
+
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY,
+            username TEXT
+        )
+        """)
+
+        conn.commit()
+        conn.close()
